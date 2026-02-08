15cbd0aafe0b455561856adc76f54fd56a6da8ca
Alifa <arofahlaila99@gmail.com>
2026-02-08 19:53:30 +0700

    Event model working: pre-save hook fixed and data successfully created

--- drizzle/0000_hesitant_leopardon.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE "products" (
-	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
-	"name" text NOT NULL,
-	"brand" text NOT NULL,
-	"price" integer NOT NULL,
-	"image" text,
-	"created_at" timestamp DEFAULT now() NOT NULL
-);
--- lib/constants.ts
+++ lib/constants.ts
@@ -96,3 +96,5 @@ export const events = [
     time: "9:00 AM - 5:00 PM",
   },
 ];
+
+//o8on9OmppsMSwZxy pass" mongodb
